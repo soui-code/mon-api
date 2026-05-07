89f1f86974274d106e596bcc30c3dcc5f67864ff
soui-code <soui0564910670.com>
2026-05-07 11:51:44 +0000

    tout nouveau

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let users = [
+  {
+    name: "Youssouf",
+    age: 22
+  }
+];
+
+// GET
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// POST
+app.post("/users", (req, res) => {
+  const user = req.body;
+
+  users.push(user);
+
+  res.json({
+    message: "Utilisateur ajouté",
+    data: user
+  });
+});
+
+
+// IMPORTANT POUR LE DEPLOIEMENT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
