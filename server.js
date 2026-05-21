a4822fdcd65e19d8b0f7b9b2a871fbbb2834e26f
soui-code <soui0564910670.com>
2026-05-21 15:09:10 +0000

    4.3MAJ

--- server.js
+++ server.js
@@ -1,62 +1,91 @@
 const express = require("express");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://soui0564910670_db_user:WLMd27UNhrtyL7Q6@cluster0.n4p87yc.mongodb.net/?appName=Cluster0")
+.then(() => console.log("MongoDB connecté"))
+.catch((error) => console.log(error));
 const app = express();
+//importation du modèle User
+const User = require("./models/User");
 
 app.use(cors());
 app.use(express.json());
 
-let users = [
-  {
-    name: "Youssouf",
-    age: 22
-  }
-];
+// let users = [
+//   {
+//     name: "Youssouf",
+//     age: 22
+//   }
+// ];
 
 // GET
-app.get("/users", (req, res) => {
-  res.json(users);
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Erreur lors de la récupération des utilisateurs" });
+  }
 });
 
 // POST
-app.post("/users", (req, res) => {
-
-  const user = req.body;
-
-  users.push(user);
-
-  res.json({
-    message: "Utilisateur ajouté",
-    data: user
-  });
-
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({
+      message: "Utilisateur ajouté",
+      data: user
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Erreur lors de l'ajout de l'utilisateur" });
+  }
 });
 
 // DELETE
-app.delete("/users/:index", (req, res) => {
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+    res.json({ message: "Utilisateur supprimé", data: user });
+  } catch (error) {
+    res.status(500).json({ message: "Erreur lors de la suppression de l'utilisateur" });
+  }
+});
 
-  const index = req.params.index;
-
-  users.splice(index, 1);
+// UPDATE
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+    res.json({ message: "Utilisateur mis à jour", data: user });
+  } catch (error) {
+    res.status(400).json({ message: "Erreur lors de la mise à jour de l'utilisateur" });
+  }
+});
 
   res.json({
     message: "Utilisateur supprimé"
   });
 
-});
+
 
 //UPDATE
-app.put("/users/:index", (req, res) => {
-  const index = req.params.index;
-  const user = req.body;
+// app.put("/users/:index", (req, res) => {
+//   const index = req.params.index;
+//   const user = req.body;
 
-  users[index] = user;
+//   users[index] = user;
 
-  res.json({
-    message: "Utilisateur mis à jour",
-    data: user
-  });
-});
+//   res.json({
+//     message: "Utilisateur mis à jour",
+//     data: user
+//   });
+// });
 
 const PORT = process.env.PORT || 3000;
 
